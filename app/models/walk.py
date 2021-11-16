da81624c37822ea3cf71e40c1abd774079aad84c
Jack Percival <jackpercival7@gmail.com>
2021-11-16 10:51:58 -0800

    Initial table db setup

--- app/models/walk.py
+++ app/models/walk.py
@@ -1,5 +1,5 @@
 from .db import db
-
+from sqlalchemy import UniqueConstraint
 
 class Walk(db.Model):
     __tablename__ = 'walks'
@@ -12,10 +12,12 @@ class Walk(db.Model):
     user = db.relationship("User", back_populates="walk")
     dog = db.relationship("Dog", back_populates="walk")
 
+    __table_args__ = (UniqueConstraint('dog_id', 'date', name='dog_walk_date'),)
+
     def to_dict(self):
         return {
             'id': self.id,
-            'user_id': self.spotId,
-            'dog_id': self.userId,
+            'user_id': self.user_id,
+            'dog_id': self.dog_id,
             'date': self.date,
         }
