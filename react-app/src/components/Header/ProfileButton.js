53c5c406b8bff06f42b28eeea1df6fec784ceea2
Jack Percival <jackpercival7@gmail.com>
2021-11-16 14:42:03 -0800

    Profile button styling

--- react-app/src/components/Header/ProfileButton.js
+++ react-app/src/components/Header/ProfileButton.js
@@ -12,22 +12,32 @@ const ProfileButton = ({ user }) => {
   return (
     <div className="profileButtonAndDropDown">
         <div className="profileButton">
-            <div className="threeLines"></div>
-            <div className="profileButtonUserIcon"></div>
-        </div>
-        <div className="profileDropDown">
+            <div className="threeLines">
+                <div></div>
+                <div></div>
+                <div></div>
+            </div>
             {user? (
-                <>
-                    <Link to="/your-dogs">Post a pup</Link>
-                    <Link to="/account-settings">Account</Link>
-                </>
+                <div className="profileButtonUserIcon" style={{backgroundImage: `url(${user.profile_pic}), url(https://res.cloudinary.com/dt8q1ngxj/image/upload/v1637102034/Capstone/noProfPic_uxrkv7.png)`}}></div>
             ) : (
-                <>
-                    <div>Log in</div>
-                    <div>Sign up</div>
-                </>
+                <div className="profileButtonUserIcon" style={{backgroundImage: `url(https://res.cloudinary.com/dt8q1ngxj/image/upload/v1637102034/Capstone/noProfPic_uxrkv7.png)`}}></div>
             )}
         </div>
+        {showProfileDropDown && (
+            <div className="profileDropDown">
+                {user? (
+                    <>
+                        <Link to="/your-dogs">Post a pup</Link>
+                        <Link to="/account-settings">Account</Link>
+                    </>
+                ) : (
+                    <>
+                        <div>Log in</div>
+                        <div>Sign up</div>
+                    </>
+                )}
+            </div>
+        )}
     </div>
   );
 }
