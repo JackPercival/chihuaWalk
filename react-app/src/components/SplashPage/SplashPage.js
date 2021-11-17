bc64dff8fae97b623b09550eb542e96c3bda409f
Jack Percival <jackpercival7@gmail.com>
2021-11-17 08:11:15 -0800

    Being splash page styling

---
+++ react-app/src/components/SplashPage/SplashPage.js
@@ -0,0 +1,31 @@
+import React, { useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { NavLink, Link } from 'react-router-dom';
+
+import './SplashPage.css'
+
+const SplashPage = () => {
+
+//   const user = useSelector(state => state.session.user);
+
+  return (
+    <div className="splashPageContainer">
+        <div className="topSpashPage">
+            <div className="meanHeaderSlogan">
+                <h1>ChihuaWalk</h1>
+                <h2>Help Dog Adoption. Schedule a Walk with a Shelter Dog.</h2>
+            </div>
+        </div>
+        <div className="notSureContainer">
+            <div className="notSureContent">
+                <h2>Not sure who to walk? Pawfect.</h2>
+                <Link to='/browse'>
+                    <div>I'm flexible</div>
+                </Link>
+            </div>
+        </div>
+    </div>
+  );
+}
+
+export default SplashPage;
