c0129eb86b63db29f3226f54bd5c19be961d56cb
Jack Percival <jackpercival7@gmail.com>
2021-11-18 15:30:03 -0800

    Update fonts

--- react-app/src/components/SingleDog/singleDog.js
+++ react-app/src/components/SingleDog/singleDog.js
@@ -14,6 +14,7 @@ function SingleDog() {
     const user = useSelector(state => state.session.user);
 
     const dog = useSelector(state => state.dogs[dogId]);
+    console.log(dog)
     const [isLoaded, setIsLoaded] = useState(false);
 
     useEffect(() => {
@@ -28,6 +29,7 @@ function SingleDog() {
             {isLoaded && (
                 <div className="singleDogContainer">
                     <h1>{dog?.name}</h1>
+                    <h3>{`${dog?.city}, ${dog?.state}, ${dog?.country}`}</h3>
                     <div className="dogImageContainer">
                         <div className="mainDogImage" style={{backgroundImage: `url(${dog?.images[0]})`}}></div>
                         <div className="smallerDogImageContainer">
