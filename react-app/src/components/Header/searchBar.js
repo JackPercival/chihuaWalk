406cbc8ad7ffd71995c00db5778fa32ac46d1816
Jack Percival <jackpercival7@gmail.com>
2021-11-22 11:07:54 -0800

    Add state selector in search bar

--- react-app/src/components/Header/searchBar.js
+++ react-app/src/components/Header/searchBar.js
@@ -7,7 +7,13 @@ import './searchBar.css'
 
 const SearchBar = () => {
 
-    const {showSearch, setShowSearch, setShowSearchClass} = useSearch();
+    const {showSearch, setShowSearch,
+        showSearchClass, setShowSearchClass,
+        searchCity, setSearchCity,
+        searchState, setSearchState,
+        searchBreed, setSearchBreed,
+        searchMinWeight, setSearchMinWeight,
+        searchMaxWeight, setSearchMaxWeight} = useSearch();
 
 
     const showRealSearch = () => {
@@ -41,19 +47,66 @@ const SearchBar = () => {
                                 type="text"
                                 autoComplete="off"
                                 placeholder="What city?"
-                                // value={email}
-                                // onChange={(e) => setEmail(e.target.value)}
+                                value={searchCity}
+                                onChange={(e) => setSearchCity(e.target.value)}
                             />
                         </div>
                         <div className="searchFormField">
                             <label>State</label>
-                            <input
-                                type="text"
-                                autoComplete="off"
-                                placeholder="What state?"
-                                // value={email}
-                                // onChange={(e) => setEmail(e.target.value)}
-                            />
+                            <select value={searchState} onChange={(e) => setSearchState(e.target.value)}>
+                                <option value="" disabled selected>What state?</option>
+                                <option value="AL">Alabama</option>
+                                <option value="AK">Alaska</option>
+                                <option value="AZ">Arizona</option>
+                                <option value="AR">Arkansas</option>
+                                <option value="CA">California</option>
+                                <option value="CO">Colorado</option>
+                                <option value="CT">Connecticut</option>
+                                <option value="DE">Delaware</option>
+                                <option value="DC">District Of Columbia</option>
+                                <option value="FL">Florida</option>
+                                <option value="GA">Georgia</option>
+                                <option value="HI">Hawaii</option>
+                                <option value="ID">Idaho</option>
+                                <option value="IL">Illinois</option>
+                                <option value="IN">Indiana</option>
+                                <option value="IA">Iowa</option>
+                                <option value="KS">Kansas</option>
+                                <option value="KY">Kentucky</option>
+                                <option value="LA">Louisiana</option>
+                                <option value="ME">Maine</option>
+                                <option value="MD">Maryland</option>
+                                <option value="MA">Massachusetts</option>
+                                <option value="MI">Michigan</option>
+                                <option value="MN">Minnesota</option>
+                                <option value="MS">Mississippi</option>
+                                <option value="MO">Missouri</option>
+                                <option value="MT">Montana</option>
+                                <option value="NE">Nebraska</option>
+                                <option value="NV">Nevada</option>
+                                <option value="NH">New Hampshire</option>
+                                <option value="NJ">New Jersey</option>
+                                <option value="NM">New Mexico</option>
+                                <option value="NY">New York</option>
+                                <option value="NC">North Carolina</option>
+                                <option value="ND">North Dakota</option>
+                                <option value="OH">Ohio</option>
+                                <option value="OK">Oklahoma</option>
+                                <option value="OR">Oregon</option>
+                                <option value="PA">Pennsylvania</option>
+                                <option value="RI">Rhode Island</option>
+                                <option value="SC">South Carolina</option>
+                                <option value="SD">South Dakota</option>
+                                <option value="TN">Tennessee</option>
+                                <option value="TX">Texas</option>
+                                <option value="UT">Utah</option>
+                                <option value="VT">Vermont</option>
+                                <option value="VA">Virginia</option>
+                                <option value="WA">Washington</option>
+                                <option value="WV">West Virginia</option>
+                                <option value="WI">Wisconsin</option>
+                                <option value="WY">Wyoming</option>
+                            </select>
                         </div>
                         <div className="searchFormField">
                             <label>Breed</label>
@@ -61,8 +114,8 @@ const SearchBar = () => {
                                 type="text"
                                 autoComplete="off"
                                 placeholder="Add a breed"
-                                // value={email}
-                                // onChange={(e) => setEmail(e.target.value)}
+                                value={searchBreed}
+                                onChange={(e) => setSearchBreed(e.target.value)}
                             />
                         </div>
                         <div className="searchFormField">
@@ -71,8 +124,8 @@ const SearchBar = () => {
                                 type="text"
                                 autoComplete="off"
                                 placeholder="Minimum weight"
-                                // value={email}
-                                // onChange={(e) => setEmail(e.target.value)}
+                                value={searchMinWeight}
+                                onChange={(e) => setSearchMinWeight(e.target.value)}
                             />
                         </div>
                         <div className="searchFormField lastSearchCat">
@@ -81,8 +134,8 @@ const SearchBar = () => {
                                 type="text"
                                 autoComplete="off"
                                 placeholder="Maximum weight?"
-                                // value={email}
-                                // onChange={(e) => setEmail(e.target.value)}
+                                value={searchMaxWeight}
+                                onChange={(e) => setSearchMaxWeight(e.target.value)}
                             />
                         </div>
                         <div className="realSearchButtonContainer">
