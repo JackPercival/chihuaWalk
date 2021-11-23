5c4c3b53a51113b4fd5d49ad21cffc8827a086e7
Jack Percival <jackpercival7@gmail.com>
2021-11-22 22:43:49 -0800

    Fix slide issue

--- react-app/src/components/DogSlide/dogSlide.js
+++ react-app/src/components/DogSlide/dogSlide.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Slide } from 'react-slideshow-image';
+import {  Link } from 'react-router-dom';
 
 import './dogSlide.css'
 
@@ -8,11 +9,13 @@ const DogSlide = ({dog}) => {
 
     return (
         <div className="singleDogHolder" id="dogSlideID">
-            <Slide easing="ease" indicators={true} autoplay={false} cssClass="dogSlide2" transitionDuration={500}>
-                {imageArray.map(image =>
-                    <div key={`Dog_slide_${image.id}`} style={{backgroundImage: `url(${image}), url("https://res.cloudinary.com/dt8q1ngxj/image/upload/v1637621047/Capstone/dogFallBack_zbctxj.png")`}} className="dogImageSlide" id="dogImageSlideID"></div>
-                )}
-            </Slide>
+                <Slide easing="ease" indicators={true} autoplay={false} cssClass="dogSlide2" transitionDuration={500}>
+                    {imageArray.map(image =>
+                        <Link to={`/dogs/${dog.id}`} key={`Dog_Slide_Image_${image.id}`}>
+                            <div key={`Dog_slide_${image.id}`} style={{backgroundImage: `url(${image}), url("https://res.cloudinary.com/dt8q1ngxj/image/upload/v1637621047/Capstone/dogFallBack_zbctxj.png")`}} className="dogImageSlide" id="dogImageSlideID"></div>
+                        </Link>
+                    )}
+                </Slide>
         </div>
     );
 }
