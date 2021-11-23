939388141875b76328f8b7680c1c2a496aa43c77
Jack Percival <jackpercival7@gmail.com>
2021-11-22 21:26:58 -0800

    10 good dogs done

--- app/seeds/dogs.py
+++ app/seeds/dogs.py
@@ -8,13 +8,12 @@ def seed_dogs():
     dog2 = Dog(user_id=2, name="Kirby", breed="Beagle", description="Who doesn't want a cute lil beagle to walk. Kirby is such a sweatheart and will make someone very happy soon.", weight=28, address="637 Britannia Drive", city="Vallejo", state="CA", country="USA", latitude=38.10476999999999, longitude=-122.193849)
     dog3 = Dog(user_id=3, name="Maple", breed="Chihuahua", description="Such a little pup, with a big heart!", weight=8, address="5601 West Crocus Drive", city="Glendale", state="AZ", country="USA", latitude=33.6152469, longitude=-112.179737)
     dog4 = Dog(user_id=4, name="Newton", breed="Vizsla", description="Newton loves long, romantic walks on the beach. Or on a sidewalk. Or anywhere. Newton just loves walks.", weight=80, address="22572 Toreador Drive", city="Salinas", state="CA", country="USA", latitude=36.602449, longitude=-121.699071)
-    dog5 = Dog(user_id=5, name="Chester", breed="Pomeranian", description="So fluffy and cute. Chester is a bit of a diva but is great on a leash", weight=14, address="49548 Road 200", city="'Neals", state="CA", country="USA", latitude=37.153463, longitude=-119.648192)
-
-    dog6 = Dog(user_id=6, name="Rudy", breed="", description="", weight=60, address="", city="", state="", country="USA", latitude=0, longitude=0)
-    dog7 = Dog(user_id=7, name="Fitz", breed="", description="", weight=60, address="", city="", state="", country="USA", latitude=0, longitude=0)
-    dog8 = Dog(user_id=8, name="Cooper", breed="", description="", weight=60, address="", city="", state="", country="USA", latitude=0, longitude=0)
-    dog9 = Dog(user_id=9, name="Tucker", breed="", description="", weight=60, address="", city="", state="", country="USA", latitude=0, longitude=0)
-    dog10 = Dog(user_id=10, name="Simba", breed="", description="", weight=60, address="", city="", state="", country="USA", latitude=0, longitude=0)
+    dog5 = Dog(user_id=5, name="Chester", breed="Pomeranian", description="So fluffy and cute. Chester is a bit of a diva but is great on a leash", weight=14, address="49548 Road 200", city="O'Neals", state="CA", country="USA", latitude=37.153463, longitude=-119.648192)
+    dog6 = Dog(user_id=6, name="Rudy", breed="Pug", description="Pugs are cute, and Rudy is no exception. Just one walk and you will fall in love", weight=19, address="20930 Todd Valley Road", city="Foresthill", state="CA", country="USA", latitude=38.989466, longitude=-120.883108)
+    dog7 = Dog(user_id=7, name="Fitz", breed="Great Dane", description="Fitz really is a great dog (it's in the name). He needs someone with excellent handling skills, but is an awesome companion", weight=130, address="125 John Street", city="Santa Cruz", state="CA", country="USA", latitude=36.950901, longitude=-122.046881)
+    dog8 = Dog(user_id=8, name="Sasha", breed="Jack Russell", description="Sasha is a one of a kind dog who will love you unconditionally.", weight=30, address="31353 Santa Elena Way", city="Union City", state="CA", country="USA", latitude=37.593981, longitude=-122.059762)
+    dog9 = Dog(user_id=9, name="Tucker", breed="Labrador", description="Tucker loves playing tennis - actually just loves chasing the balls on the court. If you like tennis, Tucker already loves you.", weight=60, address="10826 Pointe Royal Drive", city="Bakersfield", state="CA", country="USA", latitude=35.2930007, longitude=-119.1225908)
+    dog10 = Dog(user_id=10, name="Simba", breed="Golden Retriever", description="Watch Simba's glorious hair blow through the air as you take him on a walk", weight=70, address="19416 Barclay Road", city="Castro Valley", state="CA", country="USA", latitude=37.70382, longitude=-122.091054)
 
 
     db.session.add(dog1)
@@ -25,6 +24,8 @@ def seed_dogs():
     db.session.add(dog6)
     db.session.add(dog7)
     db.session.add(dog8)
+    db.session.add(dog9)
+    db.session.add(dog10)
 
 
     db.session.commit()
--- app/seeds/images.py
+++ app/seeds/images.py
@@ -3,25 +3,45 @@ from app.models import db, Image
 
 # Adds a demo user, you can add other users here if you want
 def seed_images():
-    image1 = Image(dog_id=1, url='https://images.dog.ceo/breeds/akita\/512px-Ainu-Dog.jpg')
-    image2 = Image(dog_id=1, url='https://images.dog.ceo/breeds/akita\/512px-Akita_inu.jpeg')
+    image1 = Image(dog_id=1, url='https://images.dog.ceo/breeds/akita/512px-Ainu-Dog.jpg')
+    image2 = Image(dog_id=1, url='https://images.dog.ceo/breeds/akita/512px-Akita_inu.jpeg')
     image3 = Image(dog_id=1, url='images.dog.ceo/breeds/akita/Akina_Inu_in_Riga_1.jpg')
 
-    image4 = Image(dog_id=2, url='images.dog.ceo/breeds/beagle/1271553739_Milo.jpg')
-    image5 = Image(dog_id=2, url='images.dog.ceo/breeds/beagle/1374053345_Milo.jpg')
-    image6 = Image(dog_id=2, url='images.dog.ceo/breeds/beagle/166407056_Milo.jpg')
+    image4 = Image(dog_id=2, url='https://images.dog.ceo/breeds/beagle/1271553739_Milo.jpg')
+    image5 = Image(dog_id=2, url='https://images.dog.ceo/breeds/beagle/1374053345_Milo.jpg')
+    image6 = Image(dog_id=2, url='https://images.dog.ceo/breeds/beagle/166407056_Milo.jpg')
 
-    image7 = Image(dog_id=3, url='https://images.dog.ceo/breeds/chihuahua/marto.jpg')
     image8 = Image(dog_id=3, url='https://images.dog.ceo/breeds/chihuahua/n02085620_10074.jpg')
     image9 = Image(dog_id=3, url='https://images.dog.ceo/breeds/chihuahua/n02085620_10131.jpg')
+    image7 = Image(dog_id=3, url='https://images.dog.ceo/breeds/chihuahua/marto.jpg')
 
     image10 = Image(dog_id=4, url='https://images.dog.ceo/breeds/vizsla/n02100583_10249.jpg')
     image11 = Image(dog_id=4, url='https://images.dog.ceo/breeds/vizsla/n02100583_10358.jpg')
     image12 = Image(dog_id=4, url='https://images.dog.ceo/breeds/vizsla/n02100583_10367.jpg')
 
-    image13 = Image(dog_id=5, url='https://images.dog.ceo/breeds/pomeranian/n02112018_10129.jpg')
     image14 = Image(dog_id=5, url='https://images.dog.ceo/breeds/pomeranian/n02112018_10158.jpg')
-    image15 = Image(dog_id=5, url='https://images.dog.ceo/breeds/pomeranian/n02112018_10158.jpg')
+    image13 = Image(dog_id=5, url='https://images.dog.ceo/breeds/pomeranian/n02112018_10129.jpg')
+    image15 = Image(dog_id=5, url='https://images.dog.ceo/breeds/pomeranian/n02112018_10243.jpg')
+
+    image16 = Image(dog_id=6, url='https://images.dog.ceo/breeds/pug/IMG_0226.jpg')
+    image17 = Image(dog_id=6, url='https://images.dog.ceo/breeds/pug/DSCF7495-2.jpg')
+    image18 = Image(dog_id=6, url='https://images.dog.ceo/breeds/pug/IMG_0233.jpg')
+
+    image19 = Image(dog_id=7, url='https://images.dog.ceo/breeds/dane-great/dane-2.jpg')
+    image20 = Image(dog_id=7, url='https://images.dog.ceo/breeds/dane-great/dane-4.jpg')
+    image21 = Image(dog_id=7, url='https://images.dog.ceo/breeds/dane-great/dane-1.jpg')
+
+    image22 = Image(dog_id=8, url='https://images.dog.ceo/breeds/terrier-russell/iguet3.jpeg')
+    image23 = Image(dog_id=8, url='https://images.dog.ceo/breeds/terrier-russell/iguet2.jpeg')
+    image24 = Image(dog_id=8, url='https://images.dog.ceo/breeds/terrier-russell/iguet4.jpeg')
+
+    image25 = Image(dog_id=9, url='https://images.dog.ceo/breeds/labrador/IMG_2752.jpg')
+    image26 = Image(dog_id=9, url='https://images.dog.ceo/breeds/labrador/IMG_4708.jpg')
+    image27 = Image(dog_id=9, url='https://images.dog.ceo/breeds/labrador/IMG_4709.jpg')
+
+    image28 = Image(dog_id=10, url='https://images.dog.ceo/breeds/retriever-golden/20200731_180910_200731.jpg')
+    image29 = Image(dog_id=10, url='https://images.dog.ceo/breeds/retriever-golden/20200801_174527_200801.jpg')
+    image30 = Image(dog_id=10, url='https://images.dog.ceo/breeds/retriever-golden/20200814_113907_200814.jpg')
 
     # for j in range (4,9):
     #     for i in range(1,4):
@@ -33,15 +53,30 @@ def seed_images():
     db.session.add(image4)
     db.session.add(image5)
     db.session.add(image6)
-    db.session.add(image7)
     db.session.add(image8)
     db.session.add(image9)
+    db.session.add(image7)
     db.session.add(image10)
     db.session.add(image11)
     db.session.add(image12)
-    db.session.add(image13)
     db.session.add(image14)
+    db.session.add(image13)
     db.session.add(image15)
+    db.session.add(image16)
+    db.session.add(image17)
+    db.session.add(image18)
+    db.session.add(image19)
+    db.session.add(image20)
+    db.session.add(image21)
+    db.session.add(image22)
+    db.session.add(image23)
+    db.session.add(image24)
+    db.session.add(image25)
+    db.session.add(image26)
+    db.session.add(image27)
+    db.session.add(image28)
+    db.session.add(image29)
+    db.session.add(image30)
 
     db.session.commit()
 
