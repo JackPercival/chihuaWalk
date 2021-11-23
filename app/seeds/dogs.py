4ad8baffc8f23c2686b3a340b234fdc7a2a5ebe8
Jack Percival <jackpercival7@gmail.com>
2021-11-22 22:19:43 -0800

    Add dog seeders

--- app/seeds/dogs.py
+++ app/seeds/dogs.py
@@ -1,8 +1,6 @@
 from app.models import db, Dog
-import random
 
-
-# Adds a demo user, you can add other users here if you want
+# Dog seeders
 def seed_dogs():
     dog1 = Dog(user_id=1, name="Casper", breed="Akita", description="Small and cute and loves to snuggle. Would make a wonderful walking buddy.", weight=25, address="560 Penstock Drive", city="Grass Valley", state="CA", country="USA", latitude=39.213076, longitude=-121.077583)
     dog2 = Dog(user_id=2, name="Kirby", breed="Beagle", description="Who doesn't want a cute lil beagle to walk. Kirby is such a sweatheart and will make someone very happy soon.", weight=28, address="637 Britannia Drive", city="Vallejo", state="CA", country="USA", latitude=38.10476999999999, longitude=-122.193849)
@@ -14,6 +12,16 @@ def seed_dogs():
     dog8 = Dog(user_id=8, name="Sasha", breed="Jack Russell", description="Sasha is a one of a kind dog who will love you unconditionally.", weight=30, address="31353 Santa Elena Way", city="Union City", state="CA", country="USA", latitude=37.593981, longitude=-122.059762)
     dog9 = Dog(user_id=9, name="Tucker", breed="Labrador", description="Tucker loves playing tennis - actually just loves chasing the balls on the court. If you like tennis, Tucker already loves you.", weight=60, address="10826 Pointe Royal Drive", city="Bakersfield", state="CA", country="USA", latitude=35.2930007, longitude=-119.1225908)
     dog10 = Dog(user_id=10, name="Simba", breed="Golden Retriever", description="Watch Simba's glorious hair blow through the air as you take him on a walk", weight=70, address="19416 Barclay Road", city="Castro Valley", state="CA", country="USA", latitude=37.70382, longitude=-122.091054)
+    dog11 = Dog(user_id=4, name="Widget", breed="Akita", description="Widget can walk all day. Can you keep up?", weight=35, address="21950 Arnold Center Road", city="Carson", state="CA", country="USA", latitude=33.8272706, longitude=-118.2302826)
+    dog12 = Dog(user_id=5, name="Spud", breed="Beagle", description="Spud is known for his long naps. But when he's awake he's quite the chatterbox.", weight=27, address="145 Grau Drive", city="Fremont", state="CA", country="USA", latitude=7.582453, longitude=-121.994476)
+    dog13 = Dog(user_id=6, name="Charlie", breed="Chihuahua", description="If you want a cuddle buddy, who sometimes will go for a walk, Charlie is your man.", weight=9, address="3959 Fairlands Driv", city="Pleasanton", state="CA", country="USA", latitude=37.6992001, longitude=-121.8703701)
+    dog14 = Dog(user_id=7, name="Preston", breed="Vizsla", description="Preston actually caught a car one time. And as the saying goes, he had no idea what to do next", weight=85, address="19590 East Batavia Drive", city="Aurora", state="CO", country="USA", latitude=39.7420886, longitude=-104.7581149)
+    dog15 = Dog(user_id=8, name="Sully", breed="Pomeranian", description="Sully's favorite movie is Monsters Inc. If you let him, he will watch it all day (after he goes for a walk, of course!)", weight=13, address="6424 Simms Street", city="Arvada", state="CO", country="USA", latitude=39.8133443, longitude=-105.1283237)
+    dog16 = Dog(user_id=9, name="Nala", breed="Pug", description="Nala just wants someone to love her as much as she loves walks.", weight=0, address="9036 Calico Court", city="Hesperia", state="CA", country="USA", latitude=34.414491, longitude=-117.375403)
+    dog17 = Dog(user_id=10, name="Benji", breed="Great Dane", description="If you like big dogs, Benji is the one for you", weight=180, address="90 Via Verde", city="San Lorenzo", state="CA", country="USA", latitude=37.67869, longitude=-122.117142)
+    dog18 = Dog(user_id=1, name="Neslon", breed="Labrador", description="Nelson will get you in shape. His walks are really more like brisk jogs.", weight=70, address="6231 North 59th Avenue", city="Glendale", state="AZ", country="USA", latitude=33.5285304, longitude=-112.1860744)
+    dog19 = Dog(user_id=2, name="Leo", breed="Jack Russell", description="Walking and eating are this pups' pastime", weight=32, address="553 South Arlington Road", city="Orange", state="CA", country="USA", latitude=33.7794839, longitude=-117.820383)
+    dog20 = Dog(user_id=3, name="Maverick", breed="Golden Retrieve", description="His buddies all call him Mav. If you walk him enough times, he might let you call him Mav too!", weight=63, address="457 Mountain Village Boulevard", city="Mountain Village", state="CO", country="USA", latitude=37.93323040000001, longitude=-107.8515732)
 
 
     db.session.add(dog1)
@@ -26,6 +34,16 @@ def seed_dogs():
     db.session.add(dog8)
     db.session.add(dog9)
     db.session.add(dog10)
+    db.session.add(dog11)
+    db.session.add(dog12)
+    db.session.add(dog13)
+    db.session.add(dog14)
+    db.session.add(dog15)
+    db.session.add(dog16)
+    db.session.add(dog17)
+    db.session.add(dog18)
+    db.session.add(dog19)
+    db.session.add(dog20)
 
 
     db.session.commit()
--- app/seeds/images.py
+++ app/seeds/images.py
@@ -5,7 +5,7 @@ from app.models import db, Image
 def seed_images():
     image1 = Image(dog_id=1, url='https://images.dog.ceo/breeds/akita/512px-Ainu-Dog.jpg')
     image2 = Image(dog_id=1, url='https://images.dog.ceo/breeds/akita/512px-Akita_inu.jpeg')
-    image3 = Image(dog_id=1, url='images.dog.ceo/breeds/akita/Akina_Inu_in_Riga_1.jpg')
+    image3 = Image(dog_id=1, url='images.dog.ceo/breeds/akita/Akita_hiking_in_Shpella_e_Pellumbasit.jpg')
 
     image4 = Image(dog_id=2, url='https://images.dog.ceo/breeds/beagle/1271553739_Milo.jpg')
     image5 = Image(dog_id=2, url='https://images.dog.ceo/breeds/beagle/1374053345_Milo.jpg')
@@ -43,6 +43,46 @@ def seed_images():
     image29 = Image(dog_id=10, url='https://images.dog.ceo/breeds/retriever-golden/20200801_174527_200801.jpg')
     image30 = Image(dog_id=10, url='https://images.dog.ceo/breeds/retriever-golden/20200814_113907_200814.jpg')
 
+    image31 = Image(dog_id=11, url='https://images.dog.ceo/breeds/akita/Akita_inu_blanc.jpg')
+    image32 = Image(dog_id=11, url='https://images.dog.ceo/breeds/akita/An_Akita_Inu_resting.jpg')
+    image33 = Image(dog_id=11, url='https://images.dog.ceo/breeds/akita/Japaneseakita.jpg')
+
+    image34 = Image(dog_id=12, url='https://images.dog.ceo/breeds/beagle/n02088364_11130.jpg')
+    image35 = Image(dog_id=12, url='https://images.dog.ceo/breeds/beagle/n02088364_11136.jpg')
+    image36 = Image(dog_id=12, url='https://images.dog.ceo/breeds/beagle/n02088364_11231.jpg')
+
+    image37 = Image(dog_id=13, url='https://images.dog.ceo/breeds/chihuahua/n02085620_242.jpg')
+    image38 = Image(dog_id=13, url='https://images.dog.ceo/breeds/chihuahua/n02085620_2479.jpg')
+    image39 = Image(dog_id=13, url='https://images.dog.ceo/breeds/chihuahua/n02085620_2614.jpg')
+
+    image40 = Image(dog_id=14, url='https://images.dog.ceo/breeds/vizsla/n02100583_10433.jpg')
+    image41 = Image(dog_id=14, url='https://images.dog.ceo/breeds/vizsla/n02100583_10698.jpg')
+    image42 = Image(dog_id=14, url='https://images.dog.ceo/breeds/vizsla/n02100583_10721.jpg')
+
+    image43 = Image(dog_id=15, url='https://images.dog.ceo/breeds/pomeranian/n02112018_11071.jpg')
+    image44 = Image(dog_id=15, url='https://images.dog.ceo/breeds/pomeranian/n02112018_1146.jpg')
+    image45 = Image(dog_id=15, url='https://images.dog.ceo/breeds/pomeranian/n02112018_1158.jpg')
+
+    image46 = Image(dog_id=16, url='https://images.dog.ceo/breeds/pug/c677d2fa5324.jpg')
+    image47 = Image(dog_id=16, url='https://images.dog.ceo/breeds/pug/lupita.jpg')
+    image48 = Image(dog_id=16, url='https://images.dog.ceo/breeds/pug/lupita_and_cats.jpg')
+
+    image49 = Image(dog_id=17, url='https://images.dog.ceo/breeds/dane-great/n02109047_10160.jpg')
+    image50 = Image(dog_id=17, url='https://images.dog.ceo/breeds/dane-great/n02109047_10414.jpg')
+    image51 = Image(dog_id=17, url='https://images.dog.ceo/breeds/dane-great/n02109047_11178.jpg')
+
+    image52 = Image(dog_id=18, url='https://images.dog.ceo/breeds/labrador/n02099712_1828.jpg')
+    image53 = Image(dog_id=18, url='https://images.dog.ceo/breeds/labrador/n02099712_1866.jpg')
+    image54 = Image(dog_id=18, url='https://images.dog.ceo/breeds/labrador/n02099712_209.jpg')
+
+    image55 = Image(dog_id=19, url='https://images.dog.ceo/breeds/terrier-russell/iguet5.jpeg')
+    image56 = Image(dog_id=19, url='https://images.dog.ceo/breeds/terrier-russell/jack1.jpg')
+    image57 = Image(dog_id=19, url='https://images.dog.ceo/breeds/terrier-russell/jack2.jpg')
+
+    image58 = Image(dog_id=20, url='https://images.dog.ceo/breeds/retriever-golden/n02099601_1324.jpg')
+    image59 = Image(dog_id=20, url='https://images.dog.ceo/breeds/retriever-golden/n02099601_1249.jpg')
+    image60 = Image(dog_id=20, url='https://images.dog.ceo/breeds/retriever-golden/n02099601_118.jpg')
+
     # for j in range (4,9):
     #     for i in range(1,4):
     #         db.session.add(Image(dog_id=j, url='https://www.ancarevet.com/sites/default/files/styles/large/public/labrador-retriever-dog-breed-info.jpg?itok=-Z_ky5J6'))
@@ -77,6 +117,37 @@ def seed_images():
     db.session.add(image28)
     db.session.add(image29)
     db.session.add(image30)
+    db.session.add(image31)
+    db.session.add(image32)
+    db.session.add(image33)
+    db.session.add(image34)
+    db.session.add(image35)
+    db.session.add(image36)
+    db.session.add(image37)
+    db.session.add(image38)
+    db.session.add(image39)
+    db.session.add(image40)
+    db.session.add(image41)
+    db.session.add(image42)
+    db.session.add(image43)
+    db.session.add(image44)
+    db.session.add(image45)
+    db.session.add(image46)
+    db.session.add(image47)
+    db.session.add(image48)
+    db.session.add(image49)
+    db.session.add(image50)
+    db.session.add(image51)
+    db.session.add(image52)
+    db.session.add(image53)
+    db.session.add(image54)
+    db.session.add(image55)
+    db.session.add(image56)
+    db.session.add(image57)
+    db.session.add(image58)
+    db.session.add(image59)
+    db.session.add(image60)
+
 
     db.session.commit()
 
